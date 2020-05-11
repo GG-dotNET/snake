64bd84416daa4accc93c7c578ad20095d7b034f7
GG-dotNET <ggdev.net@gmail.com>
2020-05-11 23:14:50 +0300

    created function Drow

--- Program.cs
+++ Program.cs
@@ -6,8 +6,19 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+            int x1 = 1;
+            int y1 = 3;
+            char sym = '*';
+
+            Drow(x1, y1, sym);
+
+
+        }
+
+        static void Drow(int x, int y, char sym)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(sym);
         }
     }
 }
