5ae2773457da55438d19a58dcd6d0c379956b0bd
GG-dotNET <ggdev.net@gmail.com>
2020-05-12 12:09:56 +0300

    add class Point

--- Program.cs
+++ Program.cs
@@ -6,19 +6,19 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            int x1 = 1;
-            int y1 = 3;
-            char sym = '*';
+            Point p1 = new Point();
+            p1.x = 1;
+            p1.y = 3;
+            p1.sym = '*';
+            p1.Drow();
 
-            Drow(x1, y1, sym);
-
-
+            Point p2 = new Point();
+            p2.x = 4;
+            p2.y = 5;
+            p2.sym = '#';
+            p2.Drow();
         }
 
-        static void Drow(int x, int y, char sym)
-        {
-            Console.SetCursorPosition(x, y);
-            Console.Write(sym);
-        }
+        
     }
 }
